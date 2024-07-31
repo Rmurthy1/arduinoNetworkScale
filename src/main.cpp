c19fbdbf3bb68344daac07a674474b40ae3a3bf4
Rmurthy1 <rahul.a.murthy@gmail.com>
2024-07-31 01:56:42 -0700

    working transmission over serial

--- src/main.cpp
+++ src/main.cpp
@@ -4,56 +4,95 @@
 #include <Arduino.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+#include "HX711.h"
+
+const byte rxPin = 2;
+const byte txPin = 3;
+
+// Set up a new SoftwareSerial object
+SoftwareSerial mySerial(rxPin, txPin);
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-/*
-void setup()
-{
-  lcd.init();                      // initialize the lcd 
-  lcd.init();
-  // Print a message to the LCD.
-  lcd.backlight();
-  lcd.setCursor(3,0);
-  lcd.print("Hello, world!");
-  lcd.setCursor(2,1);
-  lcd.print("Ywrobot Arduino!");
-   lcd.setCursor(0,2);
-  lcd.print("Arduino LCM IIC 2004");
-   lcd.setCursor(2,3);
-  lcd.print("Power By Ec-yuan!");
-}
-*/
-
-#include "HX711.h"
-
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 5;
-const int LOADCELL_SCK_PIN = 6;
-
 HX711 scale;
+
+const int tareButtonPin = 6; // TARE BUTTON
+
+
+const int _DoutPin = A3;
+const int _SckPin = A2;
+long _offset = 0; // tare value
+int _scale = 500;
+
+
+int period = 500;
+unsigned long time_now = 0;
+unsigned long currentTime = 0;
+
+// function declarations
+void prepareScale();
+long getValue();
+float readWeight();
+void updateScaleAndLCD();
+void clearSecondRowAndPositionCursor();
+
+float readWeight() {
+  long val = (getValue() - _offset);
+  return (float) val / _scale;
+}
+
+long getValue() {
+  uint8_t data[3];
+    long ret;
+    while (digitalRead(_DoutPin)); // wait until _dout is low
+    for (uint8_t j = 0; j < 3; j++)
+    {
+        for (uint8_t i = 0; i < 8; i++)
+        {
+            digitalWrite(_SckPin, HIGH);
+            bitWrite(data[2 - j], 7 - i, digitalRead(_DoutPin));
+            digitalWrite(_SckPin, LOW);
+        }
+    }
+
+    digitalWrite(_SckPin, HIGH);
+    digitalWrite(_SckPin, LOW);
+    ret = (((long) data[2] << 16) | ((long) data[1] << 8) | (long) data[0])^0x800000;
+    return ret;
+}
+
+void prepareScale() {
+    pinMode(_SckPin, OUTPUT);
+    pinMode(_DoutPin, INPUT);
+
+    digitalWrite(_SckPin, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(_SckPin, LOW);
+    _offset = getValue();
+}
+
+
 
 void setup() {
   Serial.begin(9600);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  mySerial.begin(9600);
+  prepareScale();
+  lcd.init();                     
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Weight: ");
+  pinMode(tareButtonPin, INPUT);
 }
+
+
+String message = "";
+bool messageReady = false;
 
 void loop() {
 
-  if (scale.is_ready()) {
-    long reading = scale.read();
-    Serial.print("HX711 reading: ");
-    Serial.println(reading);
-  } else {
-    Serial.println("HX711 not found.");
-  }
-
-  delay(1000);
-  
-}
-/*
-void loop() {
-  while (Serial.available()) { 
+  updateScaleAndLCD();
+  /*while (Serial.available()) { 
     message = Serial.readString();
     messageReady = true;
     Serial.println("wrote");
@@ -61,7 +100,36 @@ void loop() {
   if (messageReady == true) {
     //do stuff
     messageReady = false;
-    mySerial.write("message");
-  }
+    message = "<" + message + ";0" + ">";
+    mySerial.write(message.c_str());
+  }*/
+}
 
-}*/
+void updateScaleAndLCD() {
+  long scaleValue;
+   if (millis() > time_now + period) {
+    time_now = millis();
+    scaleValue = -1 * readWeight();
+    String writeValue = String(scaleValue);
+    writeValue = "<" + writeValue + ">";
+    mySerial.println(writeValue.c_str());
+    //Serial.println(scaleValue);
+    clearSecondRowAndPositionCursor();
+    lcd.print(scaleValue);
+    lcd.setCursor(8, 1);
+    lcd.print("g");
+   } 
+  
+  int tareButtonState = digitalRead(tareButtonPin);
+  if (tareButtonState == HIGH) {
+    _offset = getValue();
+  }
+}
+
+void clearSecondRowAndPositionCursor() {
+  lcd.setCursor(0,1);
+  for (int i = 0; i < 16; i++) {
+    lcd.print(" ");
+  }
+  lcd.setCursor(0,1);
+}
